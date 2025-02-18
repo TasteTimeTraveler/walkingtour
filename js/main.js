b6514d624564e3b1297f4a4e8a466bfa6c1df46f
Judith Cortes <tastetimetraveler@gmail.com>
2025-02-18 15:39:36 -0600

    pagina tours

--- js/main.js
+++ js/main.js
@@ -9,3 +9,23 @@ document.addEventListener("DOMContentLoaded", function() {
         hero.style.transform = "translateY(0)";
     }, 500);
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const tours = document.querySelectorAll(".tour");
+
+    const observer = new IntersectionObserver(
+        (entries, observer) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("show");
+                    observer.unobserve(entry.target); // Evita que se repita la animación
+                }
+            });
+        },
+        { threshold: 0.3 }
+    );
+
+    tours.forEach(tour => {
+        observer.observe(tour);
+    });
+});
