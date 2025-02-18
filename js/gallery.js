cd4bbb59b0148a7683fe5e5caf219c16494396ef
Judith Cortes <tastetimetraveler@gmail.com>
2025-02-18 16:27:05 -0600

    pagina gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,5 +1,22 @@
-const images = document.querySelectorAll(".zoom-in");
+document.addEventListener("DOMContentLoaded", function () {
+    const images = document.querySelectorAll(".gallery-item img");
+    images.forEach(img => {
+        img.addEventListener("click", function () {
+            const modal = document.createElement("div");
+            modal.classList.add("modal");
+            modal.innerHTML = `<div class="modal-content">
+                                  <span class="close">&times;</span>
+                                  <img src="${img.src}" alt="${img.alt}">
+                               </div>`;
+            document.body.appendChild(modal);
+            
+            modal.querySelector(".close").addEventListener("click", function () {
+                modal.remove();
+            });
 
-images.forEach(img => {
-    observer.observe(img);
+            modal.addEventListener("click", function (e) {
+                if (e.target === modal) modal.remove();
+            });
+        });
+    });
 });
